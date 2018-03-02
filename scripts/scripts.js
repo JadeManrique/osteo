c1009712f608f9f92996ddcd88cc2a9c4bad83d8
Jade Manrique <jade.a.manrique@gmail.com>
2018-03-01 20:59:32 -0800

    added scripts

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,3 +1,9 @@
 function password() {
-
+  var password = document.getElementById("password").value;
+  if ( password == "chickens") {
+    var argue = alert("Welcome Back, Jade!");
+    return "Correct!";
+  } else {
+    return alert("Incorrect Password");
+  }
 }
